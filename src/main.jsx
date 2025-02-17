1f02401ceb718cc8051b1a8910715ebb49929910
Agu Orcha <aguorcha@gmail.com>
2025-02-17 19:09:22 +0100

    Instalación de vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Home></Home>
+    <Analytics />
   </React.StrictMode>
 );
